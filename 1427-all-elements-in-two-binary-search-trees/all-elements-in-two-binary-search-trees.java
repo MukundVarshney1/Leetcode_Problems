040bdde7d49e6a2f3a699a2940a443ffa0d7b98a
MukundVarshney1 <mukundvarshney500@gmail.com>
2025-08-15 18:43:01 +0530

    Time: 16 ms (90.57%) | Memory: 46.2 MB (64.57%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -14,41 +14,18 @@
  * }
  */
 class Solution {
+    List<Integer> ans = new ArrayList<>();
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer> a=new ArrayList<>();
-        List<Integer> b=new ArrayList<>();
-        // Inorder always gives sorted list in bst
-        Inorder(root1,a);
-        Inorder(root2,b);
-        // merging two sorted list makes o(n+m) time complexity
-        return merge(a,b);
-    }
-    public void Inorder(TreeNode root,List<Integer> ans){
-        if(root==null){
-            return;
-        }
-        Inorder(root.left,ans);
-        ans.add(root.val);
-        Inorder(root.right,ans);
-    }
-    public List<Integer> merge(List<Integer> a,List<Integer> b){
-        int q=0;
-        int w=0;
-        List<Integer> ans=new ArrayList<>();
-        while(q<a.size() && w<b.size()){
-            if(a.get(q)<b.get(w)){
-                ans.add(a.get(q++));
-            }
-            else{
-                ans.add(b.get(w++));
-            }
-        }
-        while(q<a.size()){
-            ans.add(a.get(q++));
-        }
-        while(w<b.size()){
-            ans.add(b.get(w++));
-        }
+        helper(root1);
+        helper(root2);
+        Collections.sort(ans);//o(log(m+n))
         return ans;
+    }
+    public void helper(TreeNode root){
+        if(root==null)  return;
+        helper(root.left);
+        ans.add(root.val);
+        helper(root.right);
+
     }
 }
