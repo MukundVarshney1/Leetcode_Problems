c64b7b16f0a44efb0ada04a8649eca6f03af5a77
Mukund Varshney <mukundvarshney500@gmail.com>
2025-11-12 18:44:53 +0530

    Time: 9 ms (92.05%) | Memory: 48.5 MB (6.45%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> st=new Stack<>();
+        int[] ans=new int[nums.length];
+        Arrays.fill(ans,-1);
+        for(int i=0;i<nums.length;i++){
+            while(!st.isEmpty() && nums[st.peek()]<nums[i]){
+                ans[st.pop()]=nums[i];
+            }
+            st.push(i);
+        }
+        for(int i=0;i<nums.length;i++){
+            while(!st.isEmpty() && nums[st.peek()]<nums[i]){
+                ans[st.pop()]=nums[i];
+            }
+        }
+        return ans;
+    }
+}
