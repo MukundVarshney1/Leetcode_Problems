798ed708643b3fe6a88105f177fbf70011fd0d08
Mukund Varshney <mukundvarshney500@gmail.com>
2025-12-26 18:22:41 +0530

    Time: 192 ms (8.52%) | Memory: 103.3 MB (93.10%) - LeetSync

--- 3000-minimum-absolute-difference-between-elements-with-constraint/minimum-absolute-difference-between-elements-with-constraint.java
+++ 3000-minimum-absolute-difference-between-elements-with-constraint/minimum-absolute-difference-between-elements-with-constraint.java
@@ -1,21 +1,18 @@
 class Solution {
     public int minAbsoluteDifference(List<Integer> nums, int x) {
         int ans=Integer.MAX_VALUE;
-        TreeMap<Integer,Integer> tmap=new TreeMap<>();
-        for(int i=x;i<nums.size();i++){
-            tmap.put(nums.get(i),tmap.getOrDefault(nums.get(i),0)+1);
-        }
-        for(int i=0;i<nums.size()-x;i++){
+        TreeSet<Integer> tset=new TreeSet<>();
+        tset.add(nums.get(nums.size()-1));
+        for(int i=nums.size()-1-x;i>=0;i--){
             int target=nums.get(i);
-            if(tmap.floorKey(target)!=null){
-                ans=Math.min(ans,Math.abs(target-tmap.floorKey(target)));
+            if(tset.floor(target)!=null){
+                ans=Math.min(ans,Math.abs(target-tset.floor(target)));
             }
-            if(tmap.ceilingKey(target)!=null){
-                ans=Math.min(ans,Math.abs(target-tmap.ceilingKey(target)));
+            if(tset.ceiling(target)!=null){
+                ans=Math.min(ans,Math.abs(target-tset.ceiling(target)));
             }
-            tmap.put(nums.get(i+x),tmap.get(nums.get(i+x))-1);
-            if(tmap.get(nums.get(i+x))==0){
-                tmap.remove(nums.get(i+x));
+            if(i+x-1>=0){
+                tset.add(nums.get(i+x-1));
             }
         }
         return ans;
