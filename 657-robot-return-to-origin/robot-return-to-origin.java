1bdfcb90a30c7d4a3bf14ac46bb216fff8f9a881
Mukund Varshney <mukundvarshney500@gmail.com>
2026-04-05 09:26:13 +0530

    Time: 9 ms (11.87%) | Memory: 45.4 MB (91.72%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int a=0;
+        int b=0;
+        for(int i=0;i<moves.length();i++){
+            if(moves.charAt(i)=='U'){
+                a++;
+            }
+            else if(moves.charAt(i)=='D'){
+                a--;
+            }
+            else if(moves.charAt(i)=='R'){
+                b++;
+            }
+            else{
+                b--;
+            }
+        }
+        return a==0 && b==0;
+    }
+}
