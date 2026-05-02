02a02fe3b8bf1433516cca9787d4525957cceacc
Mukund Varshney <mukundvarshney500@gmail.com>
2026-05-02 16:19:31 +0530

    Time: 3 ms (91.63%) | Memory: 42.1 MB (56.33%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -4,13 +4,17 @@ class Solution {
         for(int i=2;i<=n;i++){
             int temp=i;
             int f=0;
+            int idx=0;
+            int c=0;
             while(temp>0){
                 int r=temp%10;
                 temp/=10;
+                c++;
                 if(r==0 || r==1 || r==8){
                     continue;
                 }
                 else if(r==3 || r==4 || r==7){
+                    idx=c-1;
                     f=0;
                     break;
                 }
@@ -20,6 +24,9 @@ class Solution {
             }
             if(f==1){
                 ans++;
+            }
+            else if(idx>0){
+                i+=Math.pow(10,idx)-1;
             }
         }
         return ans;
