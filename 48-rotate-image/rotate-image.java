a67cbfef3222545f7f2e4bfa6d0fe906f6aa507b
Mukund Varshney <mukundvarshney500@gmail.com>
2026-05-04 12:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (33.41%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverse(matrix);
+    }
+    public static void transpose(int[][] mat){
+        for(int i=0;i<mat.length;i++){
+            for(int j=i;j<mat[0].length;j++){
+                int temp=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
+            }
+        }
+    }
+    public static void reverse(int[][] mat){
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length/2;j++){
+                int temp=mat[i][j];
+                mat[i][j]=mat[i][mat[0].length-1-j];
+                mat[i][mat[0].length-1-j]=temp;
+            }
+        }
+    }
+}
