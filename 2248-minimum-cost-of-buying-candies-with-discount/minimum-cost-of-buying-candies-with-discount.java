de9afb688cb635f3a9ff2f6506bcae00039140a6
Mukund Varshney <mukundvarshney500@gmail.com>
2026-06-01 22:25:13 +0530

    Time: 6 ms (41.78%) | Memory: 44.4 MB (69.64%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int ans=0;
+        int c=0;
+        for(int i=cost.length-1;i>=0;i--){
+            c++;
+            if(c%3==0){
+                continue;
+            }
+            ans+=cost[i];
+        }
+        return ans;
+    }
+}
